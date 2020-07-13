e056e39ea6c282e0b00c38441cbbd5ad633705c6
innocent <paulinnocent04@gmail.com>
2020-07-13 15:47:15 +0100

    update

--- auctions/static/auctions/script.js
+++ auctions/static/auctions/script.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $('.menu').on('click',function () {
         $('.navb').show()
     })
+
+    $('.toggle').on('click',function () {
+        $('.navb').hide()
+    })
 })
